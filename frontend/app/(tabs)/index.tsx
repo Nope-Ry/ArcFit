99c2283cc860693bd4538b35059ec7af3a99a455
yaoruchang <ruchangyao@gmail.com>
2024-11-03 20:50:52 +0800

    init profile

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AccountInfo from "@/components/AccountInfo";
 import FunctionList from "@/components/FunctionList";
@@ -6,41 +6,43 @@ import FunctionList from "@/components/FunctionList";
 export default function ProfileScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* 个人资料卡片 */}
-      <AccountInfo
-        avatar={require("../../assets/images/icon.png")}
-        username="Ruchang Yao"
-        email="hhh"
-        onPress={() => {}}
-      />
-
-      {/* 按钮列表 */}
-      <View style={{ flex: 1 }}>
-        <FunctionList
-          items={[
-            {
-              icon: require("../../assets/images/favicon.png"),
-              text: "偏好设置",
-              onPress: () => {},
-            },
-            {
-              icon: require("../../assets/images/favicon.png"),
-              text: "照片时刻",
-              onPress: () => {},
-            },
-            {
-              icon: require("../../assets/images/favicon.png"),
-              text: "模式切换",
-              onPress: () => {},
-            },
-            {
-              icon: require("../../assets/images/favicon.png"),
-              text: "训练统计",
-              onPress: () => {},
-            },
-          ]}
+      <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+        {/* 个人资料卡片 */}
+        <AccountInfo
+          avatar={require("../../assets/images/icon.png")}
+          username="Ruchang Yao"
+          email="hhh"
+          onPress={() => {}}
         />
-      </View>
+
+        {/* 按钮列表 */}
+        <View style={{ flex: 1 }}>
+          <FunctionList
+            items={[
+              {
+                icon: require("../../assets/images/favicon.png"),
+                text: "偏好设置",
+                onPress: () => {},
+              },
+              {
+                icon: require("../../assets/images/favicon.png"),
+                text: "照片时刻",
+                onPress: () => {},
+              },
+              {
+                icon: require("../../assets/images/favicon.png"),
+                text: "模式切换",
+                onPress: () => {},
+              },
+              {
+                icon: require("../../assets/images/favicon.png"),
+                text: "训练统计",
+                onPress: () => {},
+              },
+            ]}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
