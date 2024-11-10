d3aeae9d9e3419240385c81cc69f0d3340d8a17a
Hscccccc <han-sc22@mails.tsinghua.edu.cn>
2024-11-10 22:53:48 +0800

    feat : init History Record Header

---
+++ frontend/components/training/HistoryRecordHeader.tsx
@@ -0,0 +1,11 @@
+import {View} from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+
+export default function HistoryRecordHeader() {
+
+    return (
+        <View className="">
+
+        </View>
+    );
+}
