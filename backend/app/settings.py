b276d341f054e56121e7bd876df422f2d190df95
hmk114 <2571213400@qq.com>
2024-11-13 23:26:55 +0800

    fix: fixed cors issue when request from browser

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     # 'rest_framework_simplejwt',.
     "knox",
+    "corsheaders",
     'accounts'
 ]
 
@@ -80,6 +81,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -165,3 +167,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+CORS_ALLOW_ALL_ORIGINS = True
