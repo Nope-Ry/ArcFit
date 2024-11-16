129c193ff3c9f3bc6ded21bec46461c792e8acf8
yaoruchang <ruchangyao@gmail.com>
2024-11-16 21:18:34 +0800

    res: add equipment's b_id index

--- frontend/res/corr.py
+++ frontend/res/corr.py
@@ -19,7 +19,7 @@ for j in range(1, motion_cnt+1):
         for e_id in data['e_id']:
             e_id_list_all[e_id-1].append(j)
                    
-print(e_id_list_all)
+# print(e_id_list_all)
                 
 for i in range(1, equipment_cnt+1):
     with open(f'equipment/json/{i}.json', 'r', encoding='utf-8') as f:
@@ -37,11 +37,34 @@ for j in range(1, motion_cnt+1):
         for b_id in data['b_id']:
             b_id_list_all[b_id-1].append(j)
                    
-print(b_id_list_all)
+# print(b_id_list_all)
                 
 for i in range(1, bodypart_cnt+1):
     with open(f'bodypart/json/{i}.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
         data['m_id'] = b_id_list_all[i-1]
+        with open(f'bodypart/json/{i}.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+            
+m_id_list_all = np.empty((motion_cnt, 0)).tolist()
+for j in range(1, motion_cnt+1):
+    with open(f'motion/json/{j}.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        for e_id in data['e_id']:
+            m_id_list_all[j-1].append(e_id)
+        
+        
+print(m_id_list_all.__len__())
+for i in range(1, bodypart_cnt+1):
+    with open(f'bodypart/json/{i}.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        e_id = []
+        for k in data['m_id']:
+            e_id.append(m_id_list_all[k-1])
+        e_id = list(set(sum(e_id, [])))
+        e_id.sort()
+        data['e_id'] = e_id
+        print(e_id)
+        
         with open(f'bodypart/json/{i}.json', 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
