f618666e36c29ed69d7a7988def938cab60fee8f
yaoruchang <ruchangyao@gmail.com>
2024-11-19 16:14:48 +0800

    feat: add human body

--- frontend/app/(tabs)/body.tsx
+++ frontend/app/(tabs)/body.tsx
@@ -2,15 +2,15 @@
 import { View, Text } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import InteractBody from "../../components/body/InteractBody";
 // TODO: 尝试expo three：
 // https://github.com/expo/expo-three?tab=readme-ov-file
 
 export default function BodyScreen() {
   return (
-    <SafeAreaView>
-      <Text>Body Screen</Text>
-    </SafeAreaView>
+    // <SafeAreaView>
+      <InteractBody  />
+    // </SafeAreaView>
   );
 }
 
