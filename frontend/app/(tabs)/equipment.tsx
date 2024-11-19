6dd0ba545911d17c79d2872b137fc95a7d1d83d9
Hscccccc <han-sc22@mails.tsinghua.edu.cn>
2024-11-19 21:46:40 +0800

    feat: add classification in equipment Page

--- frontend/app/(tabs)/equipment.tsx
+++ frontend/app/(tabs)/equipment.tsx
@@ -9,25 +9,34 @@ import EquipmentHeader from "../../components/equipment/EquipmentHeader";
 import EquipmentCard from "../../components/equipment/EquipmentCard";
 import { SafeAreaView } from "react-native-safe-area-context";
 import cardData from "../../res/equipment/json/comb.json";
+import bodypartData from "@/res/bodypart/json/comb.json";
 
 const { width, height } = Dimensions.get("window");
 
 export default function TrainingScreen() {
+  const [selectedEquipment, setSelectedEquipment] = useState(0);
+
+  const handleEquipmentSelect = (index) => {
+    setSelectedEquipment(index);
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       {/* 上部容器 */}
       <View style={styles.upContainer}>
-        <EquipmentHeader />
+        <EquipmentHeader OnSelect={handleEquipmentSelect}/>
       </View>
 
       {/* 图片卡片容器 */}
       <ScrollView contentContainerStyle={styles.cardContainer}>
-        {cardData.map((card, index) => (
-          <EquipmentCard
-            key={index}
-            information={card}
-          />
-        ))}
+        {cardData
+          .filter((card) => !selectedEquipment || bodypartData[selectedEquipment - 1].e_id.includes(card.e_id))
+          .map((card, index) => (
+            <EquipmentCard
+              key={index}
+              information={card}
+            />
+          ))}
       </ScrollView>
     </SafeAreaView>
   );
