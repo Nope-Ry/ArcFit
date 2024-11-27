87b07323c5a160f0f4db64eb64355c07c506d7f8
yaoruchang <ruchangyao@gmail.com>
2024-11-27 23:59:58 +0800

    fix: gif load

--- frontend/app/(tabs)/training.tsx
+++ frontend/app/(tabs)/training.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Button } from "react-native";
 import { ScrollView, TouchableOpacity } from "react-native";
 import TrainingHeader from "../../components/training/TrainingHeader";
 import ExerciseCard from "../../components/training/ExerciseCard";
@@ -24,6 +24,8 @@ import { useNavigationState } from "@react-navigation/native";
 let cnt = 0;
 
 export default function TrainingScreen() {
+  const path = FileSystem.documentDirectory;
+  // console.log(path);
   // TODO: 更改为useContext
   const [isTraining, setIsTraining] = useState(false);
   const [time, setTime] = useState(0);
@@ -84,7 +86,6 @@ export default function TrainingScreen() {
 
   // 切换显示方式
   const toggleView = () => {
-
     if (isTraining) {
       // 清空useState数据
       setM_id_list([]);
@@ -98,11 +99,11 @@ export default function TrainingScreen() {
       console.log("当前的时间为：", mins);
 
       const hist = {
-        "duration" : mins,
-        "date": new Date().toISOString().split("T")[0],
-        "time": new Date().toLocaleTimeString(),
-        "cnt": cnt, 
-        "records": []
+        duration: mins,
+        date: new Date().toISOString().split("T")[0],
+        time: new Date().toLocaleTimeString(),
+        cnt: cnt,
+        records: [],
       };
 
       cnt += 1;
@@ -110,26 +111,32 @@ export default function TrainingScreen() {
         exerSetsMap[key] = exerSetsMap[key].map(({ checked, ...rest }) => rest);
       }
 
-
-      for (let key in m_id_list){
-        const b_id = [];
+      for (let key in m_id_list) {
+        const b_id = [motionData[m_id_list[key] - 1].b_id];
         const records = {
-          "name": `练习 ${m_id_list[key]}`,
-          "m_id": m_id_list[key],
-          "b_id": b_id, // 寻找m_id到b_id的映射，未完成
-          "group": exerSetsMap[m_id_list[key]],
-          "rating": ratingMap[m_id_list[key]]
+          name: motionData[m_id_list[key] - 1].name,
+          m_id: m_id_list[key],
+          b_id: b_id, // 寻找m_id到b_id的映射，未完成
+          group: exerSetsMap[m_id_list[key]],
+          rating: ratingMap[m_id_list[key]],
         };
         hist["records"].push(records);
       }
       const stringdate = hist["date"].replace(/-/g, "_");
-      const path = FileSystem.documentDirectory + hist["date"] + "_" + hist["cnt"] + ".json";
+      const path =
+        FileSystem.documentDirectory +
+        hist["date"] +
+        "_" +
+        hist["cnt"] +
+        ".json";
       console.log(path);
-      FileSystem.writeAsStringAsync(path, JSON.stringify(hist)).then(() => {
-        console.log("写到了", path);
-      }).catch((err) => {
-        console.log(err);
-      });
+      FileSystem.writeAsStringAsync(path, JSON.stringify(hist))
+        .then(() => {
+          console.log("写到了", path);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       data.push(hist);
       setTime(0);
     }
@@ -156,7 +163,7 @@ export default function TrainingScreen() {
               key={index}
               exercise={{
                 name: `${motionData[item - 1].name}`,
-                image: motion_imgs[item - 1],
+                image: motion_imgs[item],
               }}
               exerSets={exerSetsMap[item]}
               setExerSets={(newSets) =>
@@ -174,7 +181,7 @@ export default function TrainingScreen() {
   } else {
     return (
       <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-        <HistoryRecordHeader 
+        <HistoryRecordHeader
           date={date}
           setDate={(newDate) => setDate(newDate)}
         />
