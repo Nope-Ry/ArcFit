88456402f0356aa10490d6dd7b73b85a41d24eae
hmk114 <2571213400@qq.com>
2024-12-16 20:40:17 +0800

    feat: generate json for exercise, muscle and equipment

--- backend/training/urls.py
+++ backend/training/urls.py
@@ -3,6 +3,9 @@ from .views import (
     TrainingSessionListView,
     TrainingSessionCreateView,
     TrainingSessionBulkDeleteView,
+    get_exercise_list,
+    get_muscle_list,
+    get_equipment_list,
 )
 
 urlpatterns = [
@@ -11,4 +14,7 @@ urlpatterns = [
     path(
         "delete", TrainingSessionBulkDeleteView.as_view(), name="trainingsession_delete"
     ),
+    path("exercise_list", get_exercise_list, name="get_exercise_list"),
+    path("muscle_list", get_muscle_list, name="get_muscle_list"),
+    path("equipment_list", get_equipment_list, name="get_equipment_list"),
 ]
